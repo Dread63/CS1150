b900848b5297b7472f6f2dfc046044c1091aac23
J <123434357+Dread63@users.noreply.github.com>
2023-09-22 12:08:59 -0600

    Initialized variables at begginning of code

--- DouglasJoshuaAssignment5.java
+++ DouglasJoshuaAssignment5.java
@@ -9,14 +9,25 @@ public class DouglasJoshuaAssignment5 {
 		Scanner input = new Scanner(System.in);
 		
 		// Initalizing variables to store the sum of and total number of grades entered
+		int totalGradeSum = 0;
+		int totalGradeCount = 0;
 		
 		// Initializing variables to store the highest grade and lowest grade
+		int highestGrade = 0;
+		int lowestGrade = 100;
 		
 		// Initializing variables to store total count of letter grades
+		int numberOfAGrades = 0;
+		int numberOfBGrades = 0;
+		int numberOfCGrades = 0;
+		int numberOfDGrades = 0;
+		int numberOfFGrades = 0;
 		
 		// Initializing variable to store outstanding high and low grades
+		int gradesBelow55Above95 = 0; 
 		
 		// Prompting the user for input
+		System.out.println("Enter each grade follwed by the enter key");
 		
 		// Initialzing variable to collect each grade entered
 		int enteredGrade = input.nextInt();
