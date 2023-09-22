c940b2e5703b907065e1b5835ae3b50464e67bf4
J <123434357+Dread63@users.noreply.github.com>
2023-09-22 11:50:18 -0600

    Created skeleton of assignment 5 code

--- DouglasJoshuaAssignment5.java
+++ DouglasJoshuaAssignment5.java
@@ -1,8 +1,74 @@
+import java.util.Scanner;
 
 public class DouglasJoshuaAssignment5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Creating the skeleton of the project
+		
+		//Initializing scanner
+		Scanner input = new Scanner(System.in);
+		
+		// Initalizing variables to store the sum of and total number of grades entered
+		
+		// Initializing variables to store the highest grade and lowest grade
+		
+		// Initializing variables to store total count of letter grades
+		
+		// Initializing variable to store outstanding high and low grades
+		
+		// Prompting the user for input
+		
+		// Initialzing variable to collect each grade entered
+		int enteredGrade = input.nextInt();
+		
+		while () {
+			
+			// Creating if statement to check for highest grade
+			if () {
+				
+			}
+			// Creating if statement to check for lowest grade
+			if () {
+				
+			}
+			// Creating if statement to check for outstanding high/low grades
+			if () {
+				
+			}
+			// Creating multiway to count number of each letter grade
+			if () {
+				
+			}
+			
+			else if () {
+				
+			}
+			
+			else if () {
+				
+			}
+			
+			else if () {
+				
+			}
+			
+			else {
+				
+			}
+			
+			// Creating if statement to add totals only if the negative number hasn't been reached
+			if () {
+				
+			}
+			// Creating if statement to check for errors
+			if () {
+				
+			}
+			// If no errors occured, print the user's output
+			else {
+				
+			}
+		}
 
 	}
 
