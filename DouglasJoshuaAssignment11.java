871268879f0075ffdbed17b6906cdbd5d9dbd04e
J <123434357+Dread63@users.noreply.github.com>
2023-11-14 17:44:35 -0700

    Assignment 11 Functioning and Comments Added

--- DouglasJoshuaAssignment11.java
+++ DouglasJoshuaAssignment11.java
@@ -1,3 +1,13 @@
+/*
+* Name: Joshua Douglas
+* Class: CS1150 - 002
+* Assignment #11
+* Due Date: Nov 15, 2023
+* Description: This program involves working with an array of car objects that encapsulates two other objects (FuelGauge, and Odometer). 
+* The program is designed to practice working with objects and writing out to a file, but it mainly simulates the operation of cars.
+* The primary tasks completed in the code include creating an arry of objects, simulating their operation in a lopp until all of
+* the car objects run out of gas, and displaying the cars in the order they run out of fuel.
+*/
 import java.io.IOException;
 import java.io.File;
 import java.io.PrintWriter;
@@ -5,47 +15,62 @@ import java.io.PrintWriter;
 public class DouglasJoshuaAssignment11 {
     public static void main(String[] args) throws IOException {
 
-        
+        // Initializing arrays to store cars before and after they run out of gas
         Car cars[] = new Car[5];
         Car carsOutOfGas[] = new Car[5];
 
+        // Creating cars to fill arrays and writing car details to file
         createCars(cars);
         writeCarDetailsToFile(cars);
-        printCars(cars);
 
-        simulateDriving(cars, carsOutOfGas);
-        displayCarsOutOfGas(carsOutOfGas);
-    }
- 
-    public static void simulateDriving(Car[] cars, Car[] carsOutOfGas) {
+        // Display the state of the cars before any driving occurs
+        System.out.println("Cars at start of simulation");
+        printCars(cars);
+        System.out.println();
+
+        int outOfGasIndex = 0; // Used to track the order cars run out of gas
+
+        // Running simulation until cars run out of gas
         while (checkFuelLevels(cars)) {
+
             for (int i = 0; i < cars.length; i++) {
-                if (cars[i].getFuelLevel().equals("Has Gas")) {
+
+                if (cars[i].getFuelLevel() == "Has Gas") {
+
                     double gallonsNeeded = 25 / cars[i].getMpg();
-                    if (cars[i].getFuelGauge().getGallons() >= gallonsNeeded) {
+
+                    if (gallonsNeeded <= cars[i].getFuelGauge().getGallons()) {
+
                         cars[i].getFuelGauge().decrementGallons(gallonsNeeded);
                         cars[i].getOdometer().incrementMiles(25);
-                    } else {
+                    }
+
+                    else {
+                    
                         cars[i].setFuelLevel("Empty");
-                        carsOutOfGas[i] = cars[i];
+                        carsOutOfGas[outOfGasIndex] = cars[i];
+                        outOfGasIndex++;
                     }
                 }
             }
         }
-    }
 
-    
-    public static void displayCarsOutOfGas(Car[] carsOutOfGas) {
-        System.out.println("Cars that ran out of gas");
+        // Displaying order of cars running out of gas to user
+        System.out.println("Cars ran out of gas in this order:");
+        System.out.println("----------------------------------");
+        
+        for (int i=0; i < carsOutOfGas.length; i++) {
 
-        for (Car car : carsOutOfGas) {
-            if (car != null) {
-                System.out.println(car.getOwner());
-            }
+            System.out.println("Ran out of gas: " + carsOutOfGas[i].getOwner());
         }
+
+        // Displaying updated state of cars in array
+        System.out.println();
+        System.out.println("Cars at end of simulation");
+        printCars(cars);
     }
-
-
+ 
+    // Create five different car objects
     public static void createCars(Car[] cars) {
 
         cars[0] = new Car("Donkey", "Toyota Tundra", 15, 34000, 8);
@@ -55,29 +80,35 @@ public class DouglasJoshuaAssignment11 {
         cars[4] = new Car("Dragon", "Chevy Suburban", 12, 150245, 30);
     }
 
+    // Print the Owner, Vehicle, MPG, Gallons, and Mileage of each vehicle in an array
     public static void printCars(Car[] cars) {
 
-        System.out.println("Cars at start of simulation");
-        System.out.println("-------------------------------------------------------");
-        System.out.printf("%-5s%15s%10s%20s%25s\n", "Owner", "Vehicle", "MPG", "Gallons", "Mileage");
-        System.out.println("-------------------------------------------------------");
+        System.out.println("------------------------------------------------------------------------------");
+        System.out.printf("%-5s%17s%16s%19s%14s\n", "Owner", "Vehicle", "MPG", "Gallons", "Mileage");
+        System.out.println("------------------------------------------------------------------------------");
         
         for(int i = 0; i < cars.length; i++) {
             
-            System.out.println(cars[i].getOwner() + " " + cars[i].getModel() + " " + cars[i].getMpg() + " " + cars[i].getFuelGauge().getGallons() + " " + cars[i].getOdometer().getMileage());
+            System.out.printf("%-15s%-20s%-15.2f%-14.2f%-20.2f\n", cars[i].getOwner(), cars[i].getModel(), cars[i].getMpg(), cars[i].getFuelGauge().getGallons(), 
+            cars[i].getOdometer().getMileage());
         }
     }
 
+    // Checking whether a car has gas or is empty and returning a bollean value based off this
     public static boolean checkFuelLevels(Car[] cars) {
+
         for (int i = 0; i < cars.length; i++) {
-            if (cars[i].getFuelGauge().getGallons() > 0) {
-                return true;  // At least one car still has gas
+
+            if (cars[i].getFuelLevel().equals("Has Gas")) {
+
+                return true; // At least one car still has gas
             }
         }
-        return false;  // All cars are out of gas
+
+        return false; // All cars are out of gas
     }
     
-
+    // Writing details of cars in an array to a txt file
     public static void writeCarDetailsToFile(Car[] cars) throws IOException {
 
         File fileName = new File("Assignment11.txt");
@@ -100,6 +131,7 @@ public class DouglasJoshuaAssignment11 {
     }
 }
 
+// Class defining object car with constructor and getters/setters
 class Car {
 
     private String owner;
@@ -149,6 +181,7 @@ class Car {
     }
 }
 
+// Calls defining object Odometer which contains mileage of each car object
 class Odometer {
 
     private double mileage;
@@ -167,6 +200,7 @@ class Odometer {
     }
 }
 
+// Calls defining object FuelGauge which contains gallons of gasoline in each car object
 class FuelGauge {
 
     private double gallons;
